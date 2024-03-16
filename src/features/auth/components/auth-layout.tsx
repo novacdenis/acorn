1a84207bed0e1e954ecc641d24ca70156a360978
Novac Denis <denisnovac2017@gmail.com>
2024-03-16 12:41:02 +0200

    adjust desktop styles for the auth layout

--- src/features/auth/components/auth-layout.tsx
+++ src/features/auth/components/auth-layout.tsx
@@ -14,10 +14,12 @@ export const AuthLayout: React.FC<{ children: React.ReactNode }> = ({ children }
           alt="Acorn"
           width={96}
           height={96}
-          className="h-24 w-24"
+          className="h-24 w-24 md:h-28 md:w-28"
         />
-        <h1 className="mt-8 text-2xl font-medium">Acorn</h1>
-        <h2 className="mt-0.5 text-muted-foreground">Please select an option to sign in</h2>
+        <h1 className="mt-8 text-2xl font-medium md:text-3xl">Acorn</h1>
+        <h2 className="mt-1 text-muted-foreground md:text-lg">
+          Please select an option to sign in
+        </h2>
       </header>
       <main className="mt-10 px-8">{children}</main>
     </div>
